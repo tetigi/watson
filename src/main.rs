e52c06146e3ae8b0776da12f460c1c1d6a6f3ab3
Luke Tomlin <luke.tomlin@gmail.com>
2019-07-24 20:06:55 +0100

    Add todos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+/// Fetches what you have on for today via Google calendar and reminders
+/// Start with calendar entries
+/// Then hack in a reminders integration too
+/// > watson
+/// > 0900 - Do homework
+/// > 1100 - Leave for lunch
+/// > 1800 - Interviews
+
 fn main() {
     println!("Hello, world!");
 }
